d900e0079590878beba016f767d5e0d4e5a79fe5
Rob Bayliss <rob@lastcallmedia.com>
2015-07-17 11:59:19 -0400

    More docs

--- www/js/app.js
+++ www/js/app.js
@@ -3,28 +3,13 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', [
+var app = angular.module('starter', [
   'ionic',
   'starter.controllers',
   'starter.services'
 ])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-// Use a value for this to avoid hardcoding it inside the app.
-.value('backend', 'http://talltails.lastcallmedia.com')
-
-.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('app', {
       url: '/app',
       templateUrl: 'templates/1-welcome.html',
@@ -50,4 +35,23 @@ angular.module('starter', [
     });
 
     $urlRouterProvider.otherwise("/app");
-});
+})
+
+// Ionic writes this part for us:
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+// Use a value for the REST backend to quickly toggle between
+// local and remote backend servers:
+app.value('backend', 'http://localhost/d8appdev/backend')
+//app.value('backend', 'http://talltails.lastcallmedia.com')
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,35 +1,39 @@
-angular.module('starter.controllers', [])
+var controllers = angular.module('starter.controllers', [])
 
-  /**
-   * /welcome
-   */
-  .controller('WelcomeCtrl', function($scope) {
-    // Set a variable that's accessible in the template
-    $scope.text = 'Welcome to Drupal 8!';
-  })
+/**
+ * /app
+ */
+controllers.controller('WelcomeCtrl', function($scope) {
+  // Set a variable that's accessible in the template
+  $scope.logo = 'img/talltails.png';
+});
 
-  /**
-   * /categories
-   */
-  .controller('CategoriesCtrl', function($scope, Category) {
-    $scope.categories = Category.query();
-  })
+/**
+ * /categories
+ */
+controllers.controller('CategoriesCtrl', function($scope, Category) {
 
-  /**
-   * /categories/{tid}
-   */
-  .controller('CategoryCtrl', function($scope, $stateParams, Category, Article) {
-    $scope.category = Category.get({tid: $stateParams.tid });
-    $scope.articles = Article.query({tid: $stateParams.tid});
-  })
+  $scope.categories = Category.query();
 
-  /**
-   * /articles/{nid}
-   */
-  .controller('ArticleCtrl', function($scope, $stateParams, Article, File) {
-    // We have to fetch both the article and the image.
-    $scope.article = Article.get({nid: $stateParams.nid}, function(article) {
-      // Once the article is loaded, retrieve the image.
-      $scope.image = File.get({fid: article.field_image[0].target_id});
-    });
+});
+
+/**
+ * /categories/{tid}
+ */
+controllers.controller('CategoryCtrl', function($scope, $stateParams, Category, Article) {
+
+  $scope.category = Category.get({tid: $stateParams.tid });
+  $scope.articles = Article.query({tid: $stateParams.tid});
+
+});
+
+/**
+ * /articles/{nid}
+ */
+controllers.controller('ArticleCtrl', function($scope, $stateParams, Article, File) {
+  // We have to fetch both the article and the image.
+  $scope.article = Article.get({nid: $stateParams.nid}, function(article) {
+    // Once the article is loaded, retrieve the image.
+    $scope.image = File.get({fid: article.field_image[0].target_id});
   });
+});
